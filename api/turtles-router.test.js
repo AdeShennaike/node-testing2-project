a29c85646d2cf4be9ba11368c34691bac9070084
Ade <shennaike2386@hotmail.com>
2022-04-28 22:46:49 -0400

    all test pass

--- api/turtles-router.test.js
+++ api/turtles-router.test.js
@@ -1,4 +1,4 @@
-const server = require('./server') 
+const server =require('./server') 
 const request = require('supertest')
 const db = require('../data/db-config')
 const Turtles = require('./turtles-model')
@@ -26,14 +26,24 @@ test('Testing if environment is set to testing', () => {
 })
 
 describe('Http Api Test', () => {
-    describe('Find turtles', () => {
-        test('[1] Get all turtles', () => {
-        })
-
-        test('[2] Find turtles by id', async () => {
-        })
-
-        test('[3] Add new turtle', async () => {
-        })
+    test('[1] GET /turtles', async () => {
+        let res = await request(server).get('/api/turtles')
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveLength(3)
+    })
+    
+    test('[2] GET /turtles/:id', async () => {
+        let res = await request(server).get('/api/turtles/1')
+        expect(res.body.turtle_id).toBe(1)
+        res = await request(server).get('/turtles/10')
+        expect(res.status).toBe(404)
+    })
+    
+    test('[3] POST /turtles', async () => {
+        let res = await request(server).post('/api/turtles').send({name: 'donatello', weapon: 'bo staff', color: 'purple'})
+        expect(res.body.name).toBe('donatello')
+        expect(res.body.turtle_id).toBe(4);
+        res = await Turtles.find();
+        expect(res.length).toBe(4); 
     })
 })
